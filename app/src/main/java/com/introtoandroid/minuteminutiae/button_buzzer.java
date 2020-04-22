540954f7ae22af36d4efb8d0e9e8c2da912f489f
durhambn <durhambn@g.cofc.edu>
2020-04-22 19:34:28 -0400

    fixed some build issues

--- app/src/main/java/com/introtoandroid/minuteminutiae/button_buzzer.java
+++ app/src/main/java/com/introtoandroid/minuteminutiae/button_buzzer.java
@@ -1,4 +1,14 @@
 package com.introtoandroid.minuteminutiae;
 
-public class button_buzzer {
+import android.os.Bundle;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class button_buzzer extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.button_buzzer);
+    }
 }
--- app/src/main/java/com/introtoandroid/minuteminutiae/questionScreen.java
+++ app/src/main/java/com/introtoandroid/minuteminutiae/questionScreen.java
@@ -20,16 +20,16 @@ public class questionScreen extends Activity {
         final Button promptC = (Button)findViewById(R.id.promptA);
         final Button promptD = (Button)findViewById(R.id.promptA);
 
-        String [] questions = getResources().getStringArray((R.array.questions));
-        String [] promptsA = getResources().getStringArray((R.array.answersA));
+        String [] questions = getResources().getStringArray(R.array.questions);
+        String [] promptsA = getResources().getStringArray(R.array.answersA);
         String [] promptsB = getResources().getStringArray((R.array.answersB));
         String [] promptsC = getResources().getStringArray((R.array.answersC));
         String [] promptsD = getResources().getStringArray((R.array.answersD));
         String [] answers = getResources().getStringArray((R.array.answers));
 
         Random rand = new Random();
-        int q = questions.length(); // idk why this doesnt work? can any of you see why?
-        int n = rand.nextInt(questions.length());
+        int q = questions.length; // idk why this doesnt work? can any of you see why?
+        int n = rand.nextInt(questions.length);
         questionText.setText(questions[n]);
         promptA.setText(promptsA[n]);
         promptB.setText(promptsB[n]);
